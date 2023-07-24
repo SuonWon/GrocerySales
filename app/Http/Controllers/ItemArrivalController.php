abb73e1b101a40f3800475e0e87a789623c2a0c7
Htet Wia Aung <htetwai343@gmail.com>
2023-07-24 15:56:19 +0630

    Add filter in Item Arrival

--- app/Http/Controllers/ItemArrivalController.php
+++ app/Http/Controllers/ItemArrivalController.php
@@ -20,9 +20,56 @@ class ItemArrivalController extends Controller
         $this->datetime = Date('Y-m-d H:i:s');
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $itemarrivals = ItemArrival::orderBy('CreatedDate', 'desc')->where('Status', 'N')->orwhere('Status', 'O')->get();
+        $query = ItemArrival::orderBy('ArrivalDate', 'desc');
+
+        if ($request->input('StartDate') !== null && $request->input('EndDate') !== null) {
+            $startDate = $request->input('StartDate');
+            $endDate = $request->input('EndDate');
+
+            if($endDate < $startDate){
+
+                return back()->with('warning','start date must be lower than end date');
+            }
+
+            $query->whereBetween('ArrivalDate', [$startDate, $endDate]);
+
+        } else if ($request->input('StartDate') !== null && $request->input('EndDate') == null) {
+
+            $query->where('ArrivalDate', '>=', $request->input('StartDate'));
+
+        } else if ($request->input('StartDate') == null && $request->input('EndDate') !== null) {
+
+            $query->where('ArrivalDate', '<=', $request->input('EndDate'));
+
+        } else {
+            // If both startDate and endDate are null, retrieve records for the current month
+       
+            $query->where('ArrivalDate', '>=', Carbon::now()->startOfMonth()->toDateString())
+                  ->where('ArrivalDate', '<=', Carbon::now()->endOfMonth()->toDateString());
+        
+        }
+
+        $CompleteStatus = $request->input('CompleteStatus');
+
+        if ($CompleteStatus === 'complete') {
+            $query->where('Status', 'O');
+        } else if ($CompleteStatus === 'ongoing') {
+            $query->where('Status', 'N');
+        } else if ($CompleteStatus === 'delete') {
+            $query->where('Status', 'D');
+        }
+
+        if($request->input('PlateNo') !== null){
+
+            $PlateNo = $request->input('PlateNo');
+
+            $query->where('PlateNo', 'LIKE', '%' . $PlateNo . '%');
+
+        }
+
+        $itemarrivals = $query->get();
 
         return view('purchase.itemarrival.index', [
             'itemarrivals' => $itemarrivals
